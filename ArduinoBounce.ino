008b6abf2e143b5087a68fc24a771da7c8312772
Volodymyr Tymchenko <vova.tymchenko@gmail.com>
2024-11-27 19:54:41 +0000

    expansion board

--- ArduinoBounce.ino
+++ ArduinoBounce.ino
@@ -2,36 +2,56 @@
 #include <Servo.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <TM1638plus.h>
 #include "MyMath.h"
 #include "Circle.h"
 #include "Ball.h"
 #include "Renderer.h"
 
-Servo servo;
-Adafruit_SSD1306 display = Adafruit_SSD1306(128, 64);
+Servo servo; //servo
+
+Adafruit_SSD1306 display = Adafruit_SSD1306(128, 64); //display
+
+//expansion board
+TM1638plus tm(D5, D6 , D7, false);
+byte tmButtons;
+
+int pauseButton = 0; //button
 
 Ball* ball2 = new Ball(5, 5, 6, 2.1, 5.3);
-
 Ball* ball3 = new Ball(15, 5, 14, 2.1, 6.3);
 
 void setup()
 {
+  //serial
   Serial.begin(115200);
+
+  //display
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.clearDisplay();
-  Serial << endl << "Hello World!" << endl;
+
+  //expansion board
+  tm.displayBegin(); 
+  tm.displayText("00000000");
+
+  //servo
   servo.attach(D3, 500, 2400);
+
+  //pause button
   pinMode(D4, INPUT_PULLUP);
 
+
+  Serial << endl << "Hello World!" << endl;
   AddBall(ball2);
   AddBall(ball3);
 }
 
-int button = 0;
-
 void loop()
 {
-  if (button != 0)
+  tmButtons = tm.readButtons(); // read which buttons are pressed
+  tm.displayIntNum(tmButtons);
+  /*
+  if (pauseButton != 0)
   {
     MakeFrame(display);
     display.display();
@@ -40,11 +60,11 @@ void loop()
 
   if (digitalRead(D4) == 0)
   {
-    Serial << "boo";
-    button = abs(button - 1);
-    if (button) ServoSpin(180, 0, -1);
+    tone(D8, 466.16, 1000);
+    pauseButton = abs(pauseButton - 1);
+    if (pauseButton) ServoSpin(180, 0, -1);
     else ServoSpin(0, 180, 1);
-  }
+  }*/
 }
 
 void ServoSpin(int startAngle, int endAngle, int direction) //spinDirection should be 1 or -1
